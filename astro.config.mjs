cb9a6915f25f9c5982040c9b4c06fdcf70442cbc
Greg Vallone <redskowa@gmail.com>
2025-07-29 16:34:57 -0600

    tutorial complete

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://astro-tutorial-blog-gsv19.netlify.app/",
+  integrations: [preact()]
+});
